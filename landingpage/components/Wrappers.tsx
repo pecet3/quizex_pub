424daf5a8b3316d759629cfd3edd7b6e92fecc7b
Jakub <pecet3107@gmail.com>
2025-06-17 17:47:07 +0200

    wrappers

--- landingpage/components/Wrappers.tsx
+++ landingpage/components/Wrappers.tsx
@@ -10,3 +10,47 @@ export const PaperWrapper = ({ children }: { children: React.ReactNode }) => {
     </div>
   );
 };
+export const LittlePaperWrapper = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className="paper paper-yellow m-auto
+       shadow-md shadow-gray-100 max-w-sm"
+    >
+      <div className="top-tape"></div>
+      <div className="pt-10 p-2"> {children}</div>
+    </div>
+  );
+};
+
+export const LittlePaperBlueWrapper = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className="paper paper-blue 
+       shadow-lg shadow-gray-100 "
+    >
+      <div className="top-tape"></div>
+      <div className="pt-10 p-2"> {children}</div>
+    </div>
+  );
+};
+export const PatternWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div
+      className="paper m-0
+       w-full  sm:max-w-lg max-w-md"
+    >
+      <div className="tape-section"></div>
+
+      {children}
+      <div className="tape-section"></div>
+    </div>
+  );
+};
